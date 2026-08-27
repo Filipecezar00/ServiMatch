683fdfac7da5820c084d377cd4be32116e2fe3b4
Filipecezar00 <cezarfilipe01@gmail.com>
2026-08-26 23:37:02 -0300

    atualizando

--- backend/src/testarRepository.js
+++ backend/src/testarRepository.js
@@ -3,4 +3,6 @@ import "dotenv/config";
 import { buscarPorEmail, criar } from "./modules/users/user.repository.js";
 
 const usuario = await buscarPorEmail("teste@teste.com");
+const criar_usuario = await criar("teste", "teste@gmail.com", "testando123");
 console.log(usuario);
+console.log(criar_usuario);
