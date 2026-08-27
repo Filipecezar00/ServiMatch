2cc017b76627b50c9655086916d75dd8cf04a11b
Filipecezar00 <cezarfilipe01@gmail.com>
2026-08-27 11:19:47 -0300

    atualizando

---
+++ backend/src/testarService.js
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import { autenticar } from "./modules/users/user.service";
+
+try {
+  const resultado = await autenticar("teste@gmail.com", "testando123");
+  console.log("RESULTADO 1:", resultado);
+} catch (error) {
+  console.error("Erro ao gerar resultado 1", error);
+}
+
+try {
+  const resultado_2 = await autenticar("email@gmail.com");
+  console.log("RESULTADO 2:", resultado_2);
+} catch (error) {
+  console.error("Erro ao gerar Resultado 2", error);
+}
+
+try {
+  const resultado_3 = await autenticar("teste@gmail.com", "testando");
+  console.log("RESULTADO 3:", resultado_3);
+} catch (error) {
+  console.error("Erro ao gerar Resultado 3", error);
+}
