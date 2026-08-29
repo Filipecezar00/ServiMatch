10e7d436c0fc87e5dda9bd0f8e1f54483faee12e
Filipecezar00 <cezarfilipe01@gmail.com>
2026-08-29 20:58:30 -0300

    atualizando

---
+++ backend/src/modules/services_offered/service_offered_service.js
@@ -0,0 +1,24 @@
+import AppError from "../../utils/AppError";
+import { buscarCategoriaPorId, salvar } from "./service_offered_repository";
+
+export async function criar_service(userId, titulo, descricao, categoryId) {
+  if (!titulo || !descricao) {
+    throw new AppError("Campos obrigatórios ausentes", 400);
+  }
+  if (titulo.trim().length < 3 || titulo.trim().length > 200) {
+    throw new AppError("titulo deve ter entre 3 e 200 caracteres");
+  }
+  if (descricao.trim().length < 10) {
+    throw new AppError("Descrição deve ter no mínimo 10 caracteres");
+  }
+  if (!categoryId) {
+    categoryId = null;
+  } else {
+    const categoria_Existente = await buscarCategoriaPorId(categoryId);
+    if (!categoria_Existente) {
+      throw new AppError("Categoria não encontrada", 404);
+    }
+  }
+  const dados_solicitacao = await salvar(userId, titulo, descricao, categoryId);
+  return dados_solicitacao;
+}
