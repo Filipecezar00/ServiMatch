ed0b9d5f8a9efb74f3953b81c37a13211705c55b
Filipecezar00 <cezarfilipe01@gmail.com>
2026-08-30 14:43:18 -0300

    atualizando

--- backend/src/modules/services_offered/service_offered_routes.js
+++ backend/src/modules/services_offered/service_offered_routes.js
@@ -2,9 +2,11 @@ import "dotenv/config";
 import express from "express";
 import { authMiddleware } from "../../middleware/auth.js";
 import { criar_controller } from "./service_offered_controller.js";
+import { listarAtivos_controller } from "./service_offered_controller.js";
 
 const router = express.Router();
 
 router.post("/criar", authMiddleware, criar_controller);
+router.get("/listar-ativos", listarAtivos_controller);
 
 export default router;
