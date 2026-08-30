a1e921d48995601552fbe6fd63fe489eccca6196
Filipecezar00 <cezarfilipe01@gmail.com>
2026-08-30 18:16:32 -0300

    atualizando

--- backend/src/modules/services_offered/service_offered_routes.js
+++ backend/src/modules/services_offered/service_offered_routes.js
@@ -5,6 +5,7 @@ import { criar_controller } from "./service_offered_controller.js";
 import {
   listarAtivos_controller,
   listarServicosUsuario_controller,
+  editarServico_controller,
 } from "./service_offered_controller.js";
 
 const router = express.Router();
@@ -12,5 +13,5 @@ const router = express.Router();
 router.post("/criar", authMiddleware, criar_controller);
 router.get("/listar-ativos", listarAtivos_controller);
 router.get("/listar-minhas", authMiddleware, listarServicosUsuario_controller);
-
+router.put("/:id", authMiddleware, editarServico_controller);
 export default router;
