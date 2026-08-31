5dc0cb6b117b02abd8df3957f3f594d6369cafe8
Filipecezar00 <cezarfilipe01@gmail.com>
2026-08-31 11:53:31 -0300

    atualizando

--- backend/src/modules/services_wanted/service_wanted_routes.js
+++ backend/src/modules/services_wanted/service_wanted_routes.js
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import { authMiddleware } from "../../middleware/auth";
 import { express } from "express";
+import { criar_controller } from "./service_wanted_controller";
 const router = express.Router();
+
+router.post("/criar-wanted", authMiddleware, criar_controller);
 
 export default router;
