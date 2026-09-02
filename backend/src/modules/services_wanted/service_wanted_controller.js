00342596f2ec2e453de0c273212d70d77070286b
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-01 21:08:43 -0300

    atualizando

--- backend/src/modules/services_wanted/service_wanted_controller.js
+++ backend/src/modules/services_wanted/service_wanted_controller.js
@@ -1,6 +1,7 @@
 import {
   criar_service,
   listar_ativos_service,
+  listar_meusAtivos_service,
 } from "./service_wanted_service.js";
 export async function criar_controller(req, res, next) {
   try {
@@ -17,6 +18,16 @@ export async function listar_ativos_controller(req, res, next) {
   try {
     const reposta = await listar_ativos_service();
     return res.status(200).json(reposta);
+  } catch (erro) {
+    next(erro);
+  }
+}
+
+export async function listar_meusAtivos_controller(req, res, next) {
+  try {
+    const userId = req.usuario.id;
+    const resposta = await listar_meusAtivos_service(userId);
+    return res.status(200).json(resposta);
   } catch (erro) {
     next(erro);
   }
