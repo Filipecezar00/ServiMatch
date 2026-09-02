537a5f3eb0f7bbb15d8c4dc265a57b16dccb4767
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-01 23:24:01 -0300

    atualizando

--- backend/src/modules/services_wanted/service_wanted_controller.js
+++ backend/src/modules/services_wanted/service_wanted_controller.js
@@ -3,6 +3,7 @@ import {
   listar_ativos_service,
   listar_meusAtivos_service,
   editar_servico_service,
+  editar_statusServico_service,
 } from "./service_wanted_service.js";
 export async function criar_controller(req, res, next) {
   try {
@@ -45,6 +46,23 @@ export async function editar_servico_controller(req, res, next) {
       titulo,
       descricao,
       categoryId,
+    );
+    return res.status(200).json(resposta);
+  } catch (erro) {
+    next(erro);
+  }
+}
+
+export async function editar_statusServico_controller(req, res, next) {
+  try {
+    const { id } = req.params;
+    const user_id = req.usuario.id;
+    const { novoStatus } = req.body;
+
+    const resposta = await editar_statusServico_service(
+      id,
+      user_id,
+      novoStatus,
     );
     return res.status(200).json(resposta);
   } catch (erro) {
