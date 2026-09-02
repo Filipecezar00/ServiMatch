b69a7d0e96cada440154f7f4e66768e82031562d
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-02 09:54:32 -0300

    atualizando

--- backend/src/modules/users/users.routes.js
+++ backend/src/modules/users/users.routes.js
@@ -6,7 +6,7 @@ import {
 import { authMiddleware } from "../../middleware/auth.js";
 const user_router = express.Router();
 
-user_router.post("/cadastro", authMiddleware, criarUsuario);
+user_router.post("/cadastro", criarUsuario);
 user_router.post("/login", authMiddleware, loginUsuario);
 
 export default user_router;
