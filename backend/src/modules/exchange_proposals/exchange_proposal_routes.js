45b13bfde36ab61d7cca56472c694117a03400a5
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-04 10:08:42 -0300

    atualizando

--- backend/src/modules/exchange_proposals/exchange_proposal_routes.js
+++ backend/src/modules/exchange_proposals/exchange_proposal_routes.js
@@ -1,10 +1,18 @@
 import "dotenv/config";
 import express from "express";
-import { criar_controller } from "./exchange_proposal_controller.js";
+import {
+  criar_controller,
+  mudar_status_controller,
+} from "./exchange_proposal_controller.js";
 import { authMiddleware } from "../../middleware/auth.js";
 
 const exchange_proposal_router = express.Router();
 
 exchange_proposal_router.post("/criar", authMiddleware, criar_controller);
+exchange_proposal_router.patch(
+  "/:id/status",
+  authMiddleware,
+  mudar_status_controller,
+);
 
 export default exchange_proposal_router;
