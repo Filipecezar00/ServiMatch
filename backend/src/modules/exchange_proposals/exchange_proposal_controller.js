c47adead029357bf686517fc7e1d900c52ca2d5b
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-03 22:04:23 -0300

    atualizando

--- backend/src/modules/exchange_proposals/exchange_proposal_controller.js
+++ backend/src/modules/exchange_proposals/exchange_proposal_controller.js
@@ -1,13 +1,9 @@
 import criar_service from "./exchange_proposal_service.js";
 export async function criar_controller(req, res, next) {
   try {
-    const {
-      proposer_id,
-      receiver_id,
-      offered_service_id,
-      wanted_service_id,
-      mensagem,
-    } = req.body;
+    const { receiver_id, offered_service_id, wanted_service_id, mensagem } =
+      req.body;
+    const proposer_id = req.usuario.id;
     const resposta = await criar_service(
       proposer_id,
       receiver_id,
@@ -15,7 +11,7 @@ export async function criar_controller(req, res, next) {
       wanted_service_id,
       mensagem,
     );
-    return res.status(200).json({ resposta });
+    return res.status(201).json({ resposta });
   } catch (erro) {
     next(erro);
   }
