ef04a11008a2b41768ced2ed9a03b6e3515c2f3a
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-03 21:53:13 -0300

    atualizando

--- backend/src/modules/exchange_proposals/exchange_proposal_controller.js
+++ backend/src/modules/exchange_proposals/exchange_proposal_controller.js
@@ -1,1 +1,22 @@
-async function criar_controller() {}
+import criar_service from "./exchange_proposal_service.js";
+export async function criar_controller(req, res, next) {
+  try {
+    const {
+      proposer_id,
+      receiver_id,
+      offered_service_id,
+      wanted_service_id,
+      mensagem,
+    } = req.body;
+    const resposta = await criar_service(
+      proposer_id,
+      receiver_id,
+      offered_service_id,
+      wanted_service_id,
+      mensagem,
+    );
+    return res.status(200).json({ resposta });
+  } catch (erro) {
+    next(erro);
+  }
+}
