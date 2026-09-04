09790fff0ab97595da319b518a9d4c6f46bc7e8b
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-03 21:57:52 -0300

    atualizando

---
+++ backend/src/modules/exchange_proposals/exchange_proposal_routes.js
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import express from "express";
+import { criar_controller } from "./exchange_proposal_controller.js";
+import { authMiddleware } from "../../middleware/auth.js";
+
+const exchange_proposal_router = express.Router();
+
+exchange_proposal_router.post("/criar", authMiddleware, criar_controller);
