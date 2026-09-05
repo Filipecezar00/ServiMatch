09f55781760f9064405cd550bf8fe2da5d7e1944
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-05 20:43:56 -0300

    atualizando

--- backend/src/modules/exchanges/exchanges.service.js
+++ backend/src/modules/exchanges/exchanges.service.js
@@ -2,6 +2,7 @@ import AppError from "../../utils/AppError.js";
 import {
   listar_exchanges_repository,
   buscar_dados_proposta,
+  update_exchange_repository,
 } from "./exchanges.repository.js";
 
 export async function listar_exchanges_service(usuarioId) {
@@ -22,6 +23,60 @@ export async function buscar_exchange_service(exchangeid, usuarioId) {
   if (!resposta) {
     throw new AppError("Não foi possivel localizar dados dessa proposta", 404);
   }
+
+  return resposta;
+}
+
+export async function update_exchange_service(
+  id,
+  status,
+  schedule_date,
+  location,
+  notes,
+  exchangeid,
+  usuarioId,
+) {
+  if (!id) {
+    throw new AppError("Não existe um identificador para essa proposta", 404);
+  }
+  if (!usuarioId) {
+    throw new AppError("Usuário invalido para executar essa ação", 400);
+  }
+  if (!exchangeid) {
+    throw new AppError("Troca não localizada", 404);
+  }
+  const exchange_data = await buscar_dados_proposta(exchangeid, usuarioId);
+  if (!exchange_data) {
+    throw new AppError("Essa troca não existe no banco", 404);
+  }
+
+  if (status !== null && status !== undefined) {
+    if (!status) {
+      throw new AppError("Categoria de status inválida", 400);
+    }
+  }
+  if (schedule_date !== null && schedule_date !== undefined) {
+    if (!schedule_date) {
+      throw new AppError("Data de agendamento inválida", 400);
+    }
+  }
+  if (location !== null && location !== undefined) {
+    if (!location) {
+      throw new AppError("Localização inválida", 400);
+    }
+  }
+  if (notes !== null && notes !== undefined) {
+    if (!notes) {
+      throw new AppError("Descrição inválida", 400);
+    }
+  }
+  const resposta = await update_exchange_repository(
+    id,
+    status,
+    schedule_date,
+    location,
+    notes,
+  );
 
   return resposta;
 }
