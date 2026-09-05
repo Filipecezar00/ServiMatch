ba0658c57c58047a5695f7c8957929922067d098
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-05 19:45:56 -0300

    atualizando

--- backend/src/modules/exchanges/exchanges.service.js
+++ backend/src/modules/exchanges/exchanges.service.js
@@ -1,10 +1,27 @@
 import AppError from "../../utils/AppError.js";
-import { listar_exchanges_repository } from "./exchanges.repository.js";
+import {
+  listar_exchanges_repository,
+  buscar_dados_proposta,
+} from "./exchanges.repository.js";
 
 export async function listar_exchanges_service(usuarioId) {
   if (!usuarioId) {
     throw new AppError("Usuário não autenticado", 400);
   }
   const resposta = await listar_exchanges_repository(usuarioId);
+  return resposta;
+}
+
+export async function buscar_exchange_service(exchangeid, usuarioId) {
+  if (!exchangeid || !usuarioId) {
+    throw new AppError("Não foi possivel atualizar essa troca", 400);
+  }
+
+  const resposta = await buscar_dados_proposta(exchangeid, usuarioId);
+
+  if (!resposta) {
+    throw new AppError("Não foi possivel localizar dados dessa proposta", 404);
+  }
+
   return resposta;
 }
