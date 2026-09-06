a693dbf9019d7487f9d0e2102eb9c80a29a706aa
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-05 21:20:46 -0300

    atualizando

--- backend/src/modules/exchanges/exchanges.routes.js
+++ backend/src/modules/exchanges/exchanges.routes.js
@@ -1,10 +1,18 @@
 import "dotenv/config";
 import express from "express";
 import { authMiddleware } from "../../middleware/auth.js";
-import { listar_exchange_controller } from "./exchanges.controller.js";
+import {
+  listar_exchange_controller,
+  update_exchange_controller,
+} from "./exchanges.controller.js";
 
 const exchange_router = express.Router();
 
 exchange_router.get("/listar", authMiddleware, listar_exchange_controller);
+exchange_router.patch(
+  "/:id/editar",
+  authMiddleware,
+  update_exchange_controller,
+);
 
 export default exchange_router;
