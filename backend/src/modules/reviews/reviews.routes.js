4c8b680c92634af80beba9d43a2332d76331fa6e
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-07 22:52:50 -0300

    atualizando

--- backend/src/modules/reviews/reviews.routes.js
+++ backend/src/modules/reviews/reviews.routes.js
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import express from "express";
 import authMiddleware from "../../middleware/auth.js";
-import { criar_review_controller } from "./reviews.controller.js";
+import {
+  criar_review_controller,
+  listar_reviews_controller,
+} from "./reviews.controller.js";
 
 const review_router = express.Router();
 
 review_router.post("/criar/:id", authMiddleware, criar_review_controller);
-
+review_router.get("/:reviewedId/reviews", listar_reviews_controller);
 export default review_router;
