d2d285fd9a552b066e40d2af9eb74248f1328672
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-07 21:08:12 -0300

    atualizando

--- backend/src/modules/reviews/reviews.routes.js
+++ backend/src/modules/reviews/reviews.routes.js
@@ -5,6 +5,6 @@ import { criar_review_controller } from "./reviews.controller.js";
 
 const review_router = express.Router();
 
-review_router.post("/criar", authMiddleware, criar_review_controller);
+review_router.post("/criar/:id", authMiddleware, criar_review_controller);
 
 export default review_router;
