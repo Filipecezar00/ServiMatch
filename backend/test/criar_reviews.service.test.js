894098094af3d718522d4f3d31e2fa634b9afc52
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-09 17:04:17 -0300

    atualizando

--- backend/test/criar_reviews.service.test.js
+++ backend/test/criar_reviews.service.test.js
@@ -20,4 +20,11 @@ describe("criar_reviews_service", () => {
 
     expect(resultado).toBeDefined();
   });
+  test("exchange não encontrada", async () => {
+    reviewsRepository.buscar_dados_review_repository.mockResolvedValue(null);
+
+    await expect(
+      criar_review_service(1, 2, 3, 5, "Testando review"),
+    ).rejects.toThrow("Erro ao buscar dados dessa solicitação");
+  });
 });
