7ccd0c6dba4720881d8f2277449df7fa19364667
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-13 18:26:38 -0300

    atualizando

--- frontend/servimatch/src/screens/RegisterScreen.tsx
+++ frontend/servimatch/src/screens/RegisterScreen.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { AuthStackParamList } from "../types/types";
+import { useNavigation } from "@react-navigation/native";
 
+type RegisterScreenNavigationProp = NativeStackNavigationProp<
+  AuthStackParamList,
+  "Register"
+>;
 export function RegisterScreen() {
+  const navigation = useNavigation<RegisterScreenNavigationProp>();
   return (
     <View>
       <Text>Tela de Cadastro</Text>
+      <Button
+        title="Já possuo uma conta"
+        onPress={() => navigation.navigate("Login")}
+      ></Button>
     </View>
   );
 }
