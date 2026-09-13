1d4e9c5d1d6b531552be4541d5b0808b121ee96a
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-13 01:06:42 -0300

    atualizando

--- backend/src/modules/users/users.routes.js
+++ backend/src/modules/users/users.routes.js
@@ -5,6 +5,41 @@ import {
 } from "../../modules/users/users.controller.js";
 const user_router = express.Router();
 
+/**
+ * @openapi
+ * /api/users/cadastro:
+ *    post:
+ *     summary: Cadastra usuário
+ *     tags:
+ *       - users
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - nome
+ *               - email
+ *               - senha
+ *             properties:
+ *               nome:
+ *                 type: string
+ *                 description: Nome do usuário
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: Endereço de e-mail do usuário
+ *               senha:
+ *                 type: string
+ *                 format: password
+ *                 description: Senha do usuário
+ *    responses:
+ *         201:
+ *           description: Usuário cadastrado com sucesso
+ *         400:
+ *           description: Dados de entrada inválidos ou ausentes
+ */
 user_router.post("/cadastro", criarUsuario);
 user_router.post("/login", loginUsuario);
 
