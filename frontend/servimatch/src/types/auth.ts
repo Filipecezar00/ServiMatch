398853a845ef7cf9eba3ef2804724f0b54aa35f5
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-13 20:54:32 -0300

    atualizando

---
+++ frontend/servimatch/src/types/auth.ts
@@ -0,0 +1,12 @@
+export interface Usuario {
+  id: number;
+  nome: string;
+  email: string;
+}
+
+export interface AuthState {
+  usuario: Usuario | null;
+  token: string | null;
+  login: (usuario: Usuario, token: string) => void;
+  logout: () => void;
+}
