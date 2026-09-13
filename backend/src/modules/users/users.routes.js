6226fa784a252876ae18d5901042a4f797d36951
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-13 01:21:37 -0300

    criando yaml do login

--- backend/src/modules/users/users.routes.js
+++ backend/src/modules/users/users.routes.js
@@ -41,6 +41,37 @@ const user_router = express.Router();
  *           description: Dados de entrada inválidos ou ausentes
  */
 user_router.post("/cadastro", criarUsuario);
+/**
+ * @openapi
+ * /api/users/login:
+ *    post:
+ *     summary: Login do usuário
+ *     tags:
+ *       - users
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - senha
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: Email do usuário
+ *               senha:
+ *                 type: string
+ *                 format: password
+ *                 description: senha do usuário
+ *    responses:
+ *         200:
+ *           description: Usuario logado
+ *         400:
+ *           description: Dados de entrada inválidos
+ */
 user_router.post("/login", loginUsuario);
 
 export default user_router;
