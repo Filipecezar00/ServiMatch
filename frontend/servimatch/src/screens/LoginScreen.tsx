c9b5cec313168b30d989c509f6b7b0ee6a284738
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-13 18:14:48 -0300

    atualizando

--- frontend/servimatch/src/screens/LoginScreen.tsx
+++ frontend/servimatch/src/screens/LoginScreen.tsx
@@ -1,10 +1,23 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { AuthStackParamList } from "../types/types";
+import { useNavigation } from "@react-navigation/native";
 
-export async function LoginScreen() {
+type LoginScreenNavigationProp = NativeStackNavigationProp<
+  AuthStackParamList,
+  "Login"
+>;
+
+export default async function LoginScreen() {
+  const navigation = useNavigation<LoginScreenNavigationProp>();
   return (
     <View>
       <Text>Tela de login</Text>
+      <Button
+        onPress={() => navigation.navigate("Register")}
+        title="Não tenho uma conta"
+      />
     </View>
   );
 }
