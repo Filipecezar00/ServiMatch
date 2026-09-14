e114c8271088481e2452d56cbb8cdae3005c6aaf
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-13 21:51:54 -0300

    atualizando

---
+++ frontend/servimatch/src/stores/useAuthStore.ts
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import { AuthState } from "../types/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      usuario: null,
+      token: null,
+
+      login: (usuario, token) => set({ usuario, token }),
+      logout: () => set({ usuario: null, token: null }),
+    }),
+    {
+      name: "@auth_store",
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
