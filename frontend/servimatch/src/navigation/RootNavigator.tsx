d1a2d0f0b00fadbc2581399cd6b7d5466a389dbc
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-13 23:28:32 -0300

    atualizando

--- frontend/servimatch/src/navigation/RootNavigator.tsx
+++ frontend/servimatch/src/navigation/RootNavigator.tsx
@@ -1,8 +1,28 @@
-import { useState } from "react";
-import { AppNavigator } from "../navigation/AppStack";
-import { AuthNavigator } from "../navigation/AuthStack";
+import { AppNavigator } from "./AppStack";
+import { AuthNavigator } from "./AuthStack";
+import { View, ActivityIndicator } from "react-native";
+import { useAuthStore } from "../stores/useAuthStore";
+import { useEffect, useState } from "react";
 
 export default function RootNavigator() {
-  const [logado, setLogado] = useState(false);
-  return logado ? <AppNavigator /> : <AuthNavigator />;
+  const [IsHidration, SetIsHidration] = useState(false);
+  const token = useAuthStore((state) => state.token);
+  useEffect(() => {
+    if (useAuthStore.persist.hasHydrated()) {
+      SetIsHidration(true);
+    } else {
+      const unsubscribe = useAuthStore.persist.onFinishHydration(() => {
+        SetIsHidration(true);
+      });
+      return () => unsubscribe();
+    }
+  }, []);
+
+  if (!IsHidration) {
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <ActivityIndicator size="large" color="#0000ff" />
+    </View>;
+  }
+
+  return token ? <AppNavigator /> : <AuthNavigator />;
 }
