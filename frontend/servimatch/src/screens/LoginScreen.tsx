b657c8bec90a918be866026e60e11c400b701b7c
Filipecezar00 <cezarfilipe01@gmail.com>
2026-09-14 23:47:14 -0300

    atualizando

--- frontend/servimatch/src/screens/LoginScreen.tsx
+++ frontend/servimatch/src/screens/LoginScreen.tsx
@@ -1,19 +1,25 @@
-import React, { use, useState } from "react";
-import { View, Text, Button, Alert, Platform } from "react-native";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  Alert,
+  TextInput,
+  Pressable,
+  KeyboardAvoidingView,
+  ActivityIndicator,
+} from "react-native";
 import { login } from "../api/auth";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { AuthStackParamList } from "../types/types";
 import { useNavigation } from "@react-navigation/native";
 import { useAuthStore } from "../stores/useAuthStore";
-import { KeyboardAvoidingView } from "react-native/types_generated/index";
-import { ActivityIndicator } from "react-native/types_generated/index";
 
 type LoginScreenNavigationProp = NativeStackNavigationProp<
   AuthStackParamList,
   "Login"
 >;
 
-export default async function LoginScreen() {
+export default function LoginScreen() {
   const [isEmail, setIsEmail] = useState("");
   const [isSenha, setIsSenha] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -21,20 +27,20 @@ export default async function LoginScreen() {
 
   const global_session = useAuthStore((state) => state.login);
 
-  const handleLogin = async (email: string, senha: string) => {
-    if (!email || email.trim().length < 10) {
+  const handleLogin = async () => {
+    if (!isEmail || isEmail.trim().length < 10) {
       Alert.alert("Preencha todos os campos");
       return setErrorMessage(
         "ERRO: O email precisa ter no mínimo 10 caracteres",
       );
     }
-    if (!senha || senha.trim().length < 8) {
+    if (!isSenha || isSenha.trim().length < 8) {
       console.log("Preencha todos os campos");
       return setErrorMessage("A senha precisa ter no mínimo 8 caracteres");
     }
     setIsLoading(true);
     try {
-      const req_login = await login(email, senha);
+      const req_login = await login(isEmail, isSenha);
 
       global_session(req_login.usuario, req_login.token);
     } catch (error) {
@@ -49,23 +55,23 @@ export default async function LoginScreen() {
   return (
     <KeyboardAvoidingView>
       <View>
-        <input
+        <TextInput
           placeholder="E-mail"
           value={isEmail}
-          onChange={setIsEmail}
-          keyboardType="email-addres"
+          onChangeText={setIsEmail}
+          keyboardType="email-address"
           autoCapitalize="none"
         />
-        <input
+        <TextInput
           placeholder="Senha"
-          value={setIsSenha}
-          onChangeTexto={setIsSenha}
+          value={isSenha}
+          onChangeText={setIsSenha}
           secureTextEntry={true}
         />
 
-        <Button onPress={handleLogin} disabled={isLoading}>
-          {isLoading ? <ActivityIndicator /> : <text>Entrar</text>}
-        </Button>
+        <Pressable onPress={handleLogin} disabled={isLoading}>
+          {isLoading ? <ActivityIndicator /> : <Text>Entrar</Text>}
+        </Pressable>
       </View>
     </KeyboardAvoidingView>
   );
