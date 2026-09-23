7814769d01cba4cbf68225d6a0a1af5841b29039
Filipe Cezar <cezarfilipe01@gmail.com>
2026-09-23 00:15:22 -0300

    atualizando

--- frontend/servimatch/src/api/serviceWanted.ts
+++ frontend/servimatch/src/api/serviceWanted.ts
@@ -1,12 +1,21 @@
 import { extractErrorMessage } from "../api/utils";
 import { api } from "../config/api";
-import { CriarServico } from "../types/service";
+import { CriarServico, Servico } from "../types/service";
 
 export async function CriarServicoWanted(
   payload: CriarServico,
 ): Promise<CriarServico> {
   try {
     const resposta_api = await api.post(`/services-wanted/criar`, payload);
+    return resposta_api.data;
+  } catch (erro) {
+    throw new Error(extractErrorMessage(erro));
+  }
+}
+
+export async function ListarServicoWante(): Promise<Servico> {
+  try {
+    const resposta_api = await api.get("/services-wanted/listar-minhas");
     return resposta_api.data;
   } catch (erro) {
     throw new Error(extractErrorMessage(erro));
