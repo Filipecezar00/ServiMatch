b83258826a83cad8924d71bc06fd18430049a208
Filipe Cezar <cezarfilipe01@gmail.com>
2026-09-23 22:27:41 -0300

    atualizando

--- frontend/servimatch/src/screens/services_wanted/CriarServicoScreen.tsx
+++ frontend/servimatch/src/screens/services_wanted/CriarServicoScreen.tsx
@@ -2,6 +2,7 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { criarServicoWanted, listarCategorias } from "../../api/serviceWanted";
 import { CriarServico } from "../../types/service";
 import { useState } from "react";
+import { extractErrorMessage } from "../../api/utils";
 
 export async function criarServico() {
   const [titulo, setTitulo] = useState("");
@@ -27,4 +28,26 @@ export async function criarServico() {
       setErroLocal(erro);
     },
   });
+
+  const handleSubmit = () => {
+    setErroLocal("");
+
+    if (!titulo || titulo.length < 5) {
+      return setErroLocal(
+        "O campo de titulo deve possuir no mínimo cinco caracteres",
+      );
+    }
+
+    if (!descricao || descricao.length < 10) {
+      return setErroLocal(
+        "O campo de descrição deve possuir no mínimo dez caracteres",
+      );
+    }
+
+    if (!categoryId) {
+      return setErroLocal("Preencha uma categoria antes de criar o Serviço");
+    }
+
+    mutate({ titulo, descricao, categoryId: categoryId });
+  };
 }
