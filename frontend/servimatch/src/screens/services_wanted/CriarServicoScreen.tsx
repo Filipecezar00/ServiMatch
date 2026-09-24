13223fdb8aa8078833d383020b813a8dd569db6c
Filipe Cezar <cezarfilipe01@gmail.com>
2026-09-23 22:19:01 -0300

    atualizando

--- frontend/servimatch/src/screens/services_wanted/CriarServicoScreen.tsx
+++ frontend/servimatch/src/screens/services_wanted/CriarServicoScreen.tsx
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { criarServicoWanted, listarCategorias } from "../../api/serviceWanted";
 import { CriarServico } from "../../types/service";
 import { useState } from "react";
@@ -8,4 +8,23 @@ export async function criarServico() {
   const [descricao, setDescricao] = useState("");
   const [categoryId, setCategoryId] = useState<number | null>(null);
   const [erroLocal, setErroLocal] = useState("");
+
+  const queryClient = useQueryClient();
+  const { isLoading } = useQuery({
+    queryKey: ["listarCategorias"],
+    queryFn: listarCategorias,
+    staleTime: 1000 * 60 * 5,
+  });
+
+  const { mutate, isPending, isSuccess } = useMutation({
+    mutationFn: criarServicoWanted,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["listarServicosProcurados"] });
+
+      navigation.back();
+    },
+    onError: (erro: string) => {
+      setErroLocal(erro);
+    },
+  });
 }
