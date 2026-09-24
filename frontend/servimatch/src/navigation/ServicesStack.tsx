6330afe8b1a2825aa09367d7c8297e71ec3c2a6d
Filipe Cezar <cezarfilipe01@gmail.com>
2026-09-24 11:53:25 -0300

    criando as telas na pilha de ServiceStack

--- frontend/servimatch/src/navigation/ServicesStack.tsx
+++ frontend/servimatch/src/navigation/ServicesStack.tsx
@@ -2,6 +2,9 @@ import { MeusServicos } from "../screens/services_offered/MeusServicosScreen";
 import { CriarServicoScreen } from "../screens/services_offered/CriarServicoScreen";
 import { EditarServico } from "../screens/services_offered/EditarServicoScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ListarServicosProcurados } from "../screens/services_wanted/MeusServicosScreen";
+import { CriarServicoWanted } from "../screens/services_wanted/CriarServicoScreen";
+import { EditarServicoWanted } from "../screens/services_wanted/EditarServicoScreen";
 
 export type ServiceStackParamList = {
   ListarMeusServicos: undefined;
@@ -20,6 +23,18 @@ export function ServicesStack() {
       <Stack.Screen component={CriarServicoScreen} name="CriarServico" />
       <Stack.Screen component={MeusServicos} name="ListarMeusServicos" />
       <Stack.Screen component={EditarServico} name="EditarServico" />
+      <Stack.Screen
+        component={ListarServicosProcurados}
+        name="ListarServicosProcurados"
+      />
+      <Stack.Screen
+        component={CriarServicoWanted}
+        name="CriarServicoProcurado"
+      />
+      <Stack.Screen
+        component={EditarServicoWanted}
+        name="EditarServicoProcurado"
+      />
     </Stack.Navigator>
   );
 }
